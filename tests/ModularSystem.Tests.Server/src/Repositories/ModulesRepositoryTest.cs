9cda652b485f190fd949e0036f25831a202ca4a8
DMokhnatkin <dmitriy.mokhnatkin@gmail.com>
2017-02-09 21:39:14 +0300

    Added tests for modules repository

--- tests/ModularSystem.Tests.Server/src/Repositories/ModulesRepositoryTest.cs
+++ tests/ModularSystem.Tests.Server/src/Repositories/ModulesRepositoryTest.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Linq;
 using ModularSystem.Common;
 using ModularSystem.Server.Repositories;
 using Moq;
@@ -21,27 +22,34 @@ namespace ModularSystem.Tests.Server.Repositories
                 Mock.Of<IModule>(
                     x =>
                         x.ModuleInfo ==
-                        new ModuleInfo(new ModuleIdentity("test", "1.0", ModuleType.Client), new ModuleInfo[0]));
+                        new ModuleInfo(new ModuleIdentity("test", "1.0", ModuleType.Server), new ModuleIdentity[0]));
             _sampleModules[1] =
                 Mock.Of<IModule>(
                     x =>
                         x.ModuleInfo ==
-                        new ModuleInfo(new ModuleIdentity("test", "1.0", ModuleType.Server), new ModuleInfo[0]));
+                        new ModuleInfo(new ModuleIdentity("test", "1.0", ModuleType.Client), new [] { _sampleModules[0].ModuleInfo.ModuleIdentity }));
             _sampleModules[2] =
                 Mock.Of<IModule>(
                     x =>
                         x.ModuleInfo ==
-                        new ModuleInfo(new ModuleIdentity("test", "2.0", ModuleType.Client), new ModuleInfo[0]));
+                        new ModuleInfo(new ModuleIdentity("test", "2.0", ModuleType.Server), new [] { _sampleModules[0].ModuleInfo.ModuleIdentity }));
             _sampleModules[3] =
                 Mock.Of<IModule>(
                     x =>
                         x.ModuleInfo ==
-                        new ModuleInfo(new ModuleIdentity("test", "2.0", ModuleType.Server), new ModuleInfo[0]));
+                        new ModuleInfo(new ModuleIdentity("test", "2.0", ModuleType.Client), new [] { _sampleModules[1].ModuleInfo.ModuleIdentity, _sampleModules[2].ModuleInfo.ModuleIdentity }));
             _sampleModules[4] =
                 Mock.Of<IModule>(
                     x =>
                         x.ModuleInfo ==
-                        new ModuleInfo(new ModuleIdentity("test2", "1.0", ModuleType.Client), new ModuleInfo[0]));
+                        new ModuleInfo(new ModuleIdentity("test2", "1.0", ModuleType.Client), new ModuleIdentity[0]));
+        }
+
+        [Test]
+        public void CheckDependencies()
+        {
+            Assert.IsFalse(_repository.CheckDependencies(_sampleModules[1].ModuleInfo).IsCheckSuccess);
+            Assert.IsFalse(_repository.CheckDependencies(_sampleModules[3].ModuleInfo).IsCheckSuccess);
         }
 
         [Test]
@@ -62,21 +70,42 @@ namespace ModularSystem.Tests.Server.Repositories
         }
 
         [Test]
+        public void TestRegisterModules()
+        {
+            _repository.RegisterModules(_sampleModules);
+            Assert.AreEqual(_sampleModules[0], _repository.GetModule(_sampleModules[0].ModuleInfo.ModuleIdentity));
+            Assert.AreEqual(_sampleModules[1], _repository.GetModule(_sampleModules[1].ModuleInfo.ModuleIdentity));
+            Assert.AreEqual(_sampleModules[2], _repository.GetModule(_sampleModules[2].ModuleInfo.ModuleIdentity));
+            Assert.AreEqual(_sampleModules[3], _repository.GetModule(_sampleModules[3].ModuleInfo.ModuleIdentity));
+            Assert.AreEqual(_sampleModules[4], _repository.GetModule(_sampleModules[4].ModuleInfo.ModuleIdentity));
+        }
+
+        [Test]
         public void TestUnregisterModule()
         {
-            _repository.RegisterModule(_sampleModules[0]);
-            _repository.RegisterModule(_sampleModules[1]);
-            _repository.RegisterModule(_sampleModules[2]);
-            _repository.RegisterModule(_sampleModules[3]);
-            _repository.RegisterModule(_sampleModules[4]);
+            _repository.RegisterModules(_sampleModules);
+
+            _repository.UnregisterModule(_sampleModules[4].ModuleInfo.ModuleIdentity);
+            _repository.UnregisterModule(_sampleModules[3].ModuleInfo.ModuleIdentity);
+            _repository.UnregisterModule(_sampleModules[2].ModuleInfo.ModuleIdentity);
+            _repository.UnregisterModule(_sampleModules[1].ModuleInfo.ModuleIdentity);
+            Assert.IsNull(_repository.GetModule(_sampleModules[1].ModuleInfo.ModuleIdentity));
+            Assert.IsNull(_repository.GetModule(_sampleModules[2].ModuleInfo.ModuleIdentity));
+            Assert.IsNull(_repository.GetModule(_sampleModules[3].ModuleInfo.ModuleIdentity));
+            Assert.IsNull(_repository.GetModule(_sampleModules[4].ModuleInfo.ModuleIdentity));
 
             _repository.UnregisterModule(_sampleModules[0].ModuleInfo.ModuleIdentity);
             Assert.IsNull(_repository.GetModule(_sampleModules[0].ModuleInfo.ModuleIdentity));
             Assert.Throws<ArgumentException>(() => _repository.UnregisterModule(_sampleModules[0].ModuleInfo.ModuleIdentity));
-            _repository.UnregisterModule(_sampleModules[1].ModuleInfo.ModuleIdentity);
-            _repository.UnregisterModule(_sampleModules[2].ModuleInfo.ModuleIdentity);
-            _repository.UnregisterModule(_sampleModules[3].ModuleInfo.ModuleIdentity);
-            _repository.UnregisterModule(_sampleModules[4].ModuleInfo.ModuleIdentity);
+        }
+
+        [Test]
+        public void TestUnregisterModules()
+        {
+            _repository.RegisterModules(_sampleModules);
+
+            _repository.UnregisterModules(_sampleModules.Select(x => x.ModuleInfo.ModuleIdentity).Reverse());
+            Assert.IsNull(_repository.GetModule(_sampleModules[0].ModuleInfo.ModuleIdentity));
             Assert.IsNull(_repository.GetModule(_sampleModules[1].ModuleInfo.ModuleIdentity));
             Assert.IsNull(_repository.GetModule(_sampleModules[2].ModuleInfo.ModuleIdentity));
             Assert.IsNull(_repository.GetModule(_sampleModules[3].ModuleInfo.ModuleIdentity));
