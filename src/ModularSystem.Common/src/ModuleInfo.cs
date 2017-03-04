a0e6bf6c86b5b3476cfcdcc021ac0b9b4ba9c694
DMokhnatkin <dmitriy.mokhnatkin@gmail.com>
2017-03-04 19:42:17 +0300

    Added docs

--- src/ModularSystem.Common/src/ModuleInfo.cs
+++ src/ModularSystem.Common/src/ModuleInfo.cs
@@ -1,9 +1,18 @@
 ﻿namespace ModularSystem.Common
 {
+    /// <summary>
+    /// Information about module. (name, dependencies, ...)
+    /// </summary>
     public class ModuleInfo
     {
+        /// <summary>
+        /// Identifier for module
+        /// </summary>
         public ModuleIdentity ModuleIdentity { get; }
 
+        /// <summary>
+        /// List of modules which are required to be installed for this module.
+        /// </summary>
         public ModuleIdentity[] Dependencies { get; }
 
         public ModuleInfo(ModuleIdentity identity, ModuleIdentity[] dependencies)
--- src/ModularSystem.Common/src/Modules/IModule.cs
+++ src/ModularSystem.Common/src/Modules/IModule.cs
@@ -1,7 +1,11 @@
 namespace ModularSystem.Common.Modules
 {
+    /// <summary>
+    /// Base module interface. All modules implement it.
+    /// </summary>
     public interface IModule
     {
+        /// <see cref="ModuleInfo"/>
         ModuleInfo ModuleInfo { get; }
     }
 }
