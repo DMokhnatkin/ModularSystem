a9526e7268d31097d712329b3840eeb1c854fe13
DMokhnatkin <dmitriy.mokhnatkin@gmail.com>
2017-03-17 23:01:04 +0300

    Added class for working with conf file

---
+++ src/ModularSystem.Common/src/ModuleConf.cs
@@ -0,0 +1,21 @@
+﻿using System.IO;
+using Newtonsoft.Json;
+
+namespace ModularSystem.Common
+{
+    public class ModuleConf
+    {
+        public string ModuleIdentity { get; set; }
+        public string[] Dependencies { get; set; }
+
+        public static ModuleConf LoadFromFile(FileStream str)
+        {
+            return JsonConvert.DeserializeObject<ModuleConf>(new StreamReader(str).ReadToEnd());
+        }
+
+        public void WriteToFile(FileStream str)
+        {
+            new StreamWriter(str).Write(JsonConvert.SerializeObject(this, Formatting.Indented));
+        }
+    }
+}
---
+++ tests/ModularSystem.Tests.Common/src/Conf/ModuleConfTest.cs
@@ -0,0 +1,45 @@
+﻿using System.IO;
+using ModularSystem.Common;
+using NUnit.Framework;
+
+namespace ModularSystem.Tests.Common.Conf
+{
+    [TestFixture]
+    public class ModuleConfTest
+    {
+        [Test]
+        public void TestLoad()
+        {
+            var t = ModuleConf.LoadFromFile(File.OpenRead("TestData/conf1.json"));
+            Assert.AreEqual(t.ModuleIdentity, "test-server-1.0");
+
+            Assert.Contains("test-client-1.1", t.Dependencies);
+            Assert.Contains("test2-server-1.5", t.Dependencies);
+            Assert.Contains("test4-client-1.7", t.Dependencies);
+        }
+
+        [Test]
+        public void TestWrite()
+        {
+            var t = new ModuleConf()
+            {
+                ModuleIdentity = "test-server-1.0",
+                Dependencies = new [] { "test-client-1.1", "test2-server-1.5", "test4-client-1.7"}
+            };
+            var s = Path.GetTempFileName();
+            using (var fs = File.OpenWrite(s))
+            {
+                t.WriteToFile(fs);
+            }
+
+            using (var fs = File.OpenRead(s))
+            {
+                var y = ModuleConf.LoadFromFile(File.OpenRead(s));
+                Assert.AreEqual(y.ModuleIdentity, "test-server-1.0");
+                Assert.Contains("test-client-1.1", y.Dependencies);
+                Assert.Contains("test2-server-1.5", y.Dependencies);
+                Assert.Contains("test4-client-1.7", y.Dependencies);
+            }
+        }
+    }
+}
