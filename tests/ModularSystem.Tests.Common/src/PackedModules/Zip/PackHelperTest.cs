85ce44094a8bbfa16bca976dd307843ccd06302d
DMokhnatkin <dmitriy.mokhnatkin@gmail.com>
2017-04-04 14:28:07 +0300

    Added test

--- tests/ModularSystem.Tests.Common/src/PackedModules/Zip/PackHelperTest.cs
+++ tests/ModularSystem.Tests.Common/src/PackedModules/Zip/PackHelperTest.cs
@@ -1,4 +1,8 @@
 ﻿using System;
+using System.IO;
+using System.Linq;
+using ModularSystem.Common;
+using ModularSystem.Common.PackedModules.Zip;
 using NUnit.Framework;
 
 namespace ModularSystem.Tests.Common.PackedModules.Zip
@@ -6,10 +10,17 @@ namespace ModularSystem.Tests.Common.PackedModules.Zip
     [TestFixture]
     public class PackHelperTest
     {
+        private static readonly string TestModule1Path = Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "TestData/Modules/module1-1.0.0");
+
         [Test]
         public void PackModuleToMemoryTest()
         {
-            
+            var r = PackHelper.PackModuleToMemory(TestModule1Path);
+            Assert.AreEqual(r.ModuleIdentity.Name, "module1");
+            Assert.AreEqual(r.ModuleIdentity.Version, new Version(1, 0, 0));
+            Assert.Contains(new ModuleIdentity("test.client.wpf", "1.1"), r.Dependencies);
+            Assert.Contains(new ModuleIdentity("test2.server", "1.5"), r.Dependencies);
+            Assert.Contains(new ModuleIdentity("test4.client.wpf", "1.7"), r.Dependencies);
         }
 
         [Test]
