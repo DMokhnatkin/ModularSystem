b72083ba402a5bb9c34217af3b864ad46bfdd6fd
DMokhnatkin <dmitriy.mokhnatkin@gmail.com>
2017-04-04 13:53:07 +0300

    Tests template

---
+++ tests/ModularSystem.Tests.Common/src/PackedModules/Zip/PackHelperTest.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using NUnit.Framework;
+
+namespace ModularSystem.Tests.Common.PackedModules.Zip
+{
+    [TestFixture]
+    public class PackHelperTest
+    {
+        [Test]
+        public void PackModuleToMemoryTest()
+        {
+            
+        }
+
+        [Test]
+        public void PackModuleToFileTest()
+        {
+            throw new NotImplementedException();
+        }
+
+        [Test]
+        public void UnpackModule()
+        {
+            throw new NotImplementedException();
+        }
+
+        [Test]
+        public void BatchModules()
+        {
+            throw new NotImplementedException();
+        }
+
+        [Test]
+        public void BatchMemoryModules()
+        {
+            throw new NotImplementedException();
+        }
+
+        [Test]
+        public void UnbatchModules()
+        {
+            throw new NotImplementedException();
+        }
+
+        [Test]
+        public void UnbatchModulesToMemory()
+        {
+            throw new NotImplementedException();
+        }
+
+        [Test]
+        public void UnbatchAndUnpackModules()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
